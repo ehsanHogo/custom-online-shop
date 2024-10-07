47bc491e0c5276ff521fa733122c447a09f9660f
ehsanHogo <ehsan.ho.go@gmail.com>
2024-10-07 11:36:21 +0330

    fix font

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p class="font-MyFont">ثبت نام ورود</p>
+  <div class="font-MyFont">
+    <div class="">
+      <p>خرید</p>
+      <img src="./assets/header/shopping-cart.png" alt="shopping cart" />
+    </div>
+    <div>
+      <p>ورود | ثبت نام</p>
+      <img src="./assets/header/Login.png" alt="" />
+    </div>
+    <div>
+      <img src="./assets/header/search-normal.png" alt="" />
+      <input type="text" />
+      <p>جستجو</p>
+    </div>
+    <div class="">
+      <div>
+        <p>کاستومی</p>
+        <p>فروشگاه اینترنتی</p>
+      </div>
+      <img src="./assets/LOGO.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
