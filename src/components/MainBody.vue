84da4843f1181a7f2eaba8064ae6a8c7b9b7b429
ehsanHogo <ehsan.ho.go@gmail.com>
2024-10-12 11:11:50 +0330

    finish filter

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const value1 = ref(false);
+const value2 = ref(false);
 const categories: string[] = [
   "پوشاک",
   "لوازم خانه",
@@ -13,6 +17,9 @@ const filterButtons: string[] = [
   "برند",
   "اندازه",
   "رنگ",
+  "ارسال امروز",
+  "فقط کالاهای موجود",
+
   "محدوده قیمت",
   "مدل",
   "طرح",
@@ -43,19 +50,22 @@ const filterButtons: string[] = [
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
 
-      <div class="col-span-1 border rounded-sm p-3 flex flex-col gap-3">
+      <div class="col-span-1 border rounded-sm p-5 flex flex-col gap-5">
         <div class="flex justify-between">
-          <p>حذف فیلتر ها</p>
-          <p>فیلتر ها</p>
+          <b class="text-redp">حذف فیلتر ها</b>
+          <b>فیلتر ها</b>
         </div>
 
-        <div class="flex flex-col gap-3">
+        <div class="flex flex-col gap-6">
           <button
             v-for="(filterButton, index) in filterButtons"
             :key="index"
             class="flex justify-between border-b last:border-none"
           >
-            <img src="../assets/body/arrow-down.png" alt="" />
+            <VaSwitch size="small" v-if="index === 3" v-model="value1" />
+            <VaSwitch size="small" v-else-if="index === 4" v-model="value2" />
+            <img v-else src="../assets/body/arrow-down.png" alt="arrow down" />
+
             <p>{{ filterButton }}</p>
           </button>
         </div>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/styles/essential.css";
+// import "vuestic-ui/styles/typography.css";
 
-createApp(App).mount('#app')
+createApp(App).use(createVuestic()).mount("#app");
+// createApp(App).mount("#app");
