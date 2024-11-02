0ef855aed74ae8bed5cf67920831ad652bb12205
ehsanHogo <ehsan.ho.go@gmail.com>
2024-11-02 14:50:18 +0330

    update router

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,17 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-import ShowProducts from "../views/ShowProducts.vue";
+import ShowProducts from "../components/products/ShowProducts.vue";
 import Page404 from "../Page404.vue";
 import ShoppingCart from "../components/shoppingCart.vue/ShoppingCart.vue";
-import MainBody from "../components/products/ShowProducts.vue";
+
 const routes: Array<RouteRecordRaw> = [
-  { path: "/custom-online-shop/", name: "products", component: MainBody },
+  { path: "/custom-online-shop/", name: "products", component: ShowProducts },
   { path: "/custom-online-shop/404", name: "404", component: Page404 },
-  {
-    path: "/custom-online-shop/shopping-cart",
-    name: "shopping-cart",
-    component: ShoppingCart,
-  },
   {
     path: "/custom-online-shop/shopping-cart",
     name: "shopping-cart",
