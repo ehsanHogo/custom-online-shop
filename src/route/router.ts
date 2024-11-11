5d7c3994db9f9c6eb9c2eef40cbc0401efa0bd7d
ehsanHogo <ehsan.ho.go@gmail.com>
2024-11-11 11:29:21 +0330

    rename shopping folder

--- src/route/router.ts
+++ src/route/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 import ShowProducts from "../components/products/ShowProducts.vue";
 import Page404 from "../Page404.vue";
-import ShoppingCart from "../components/shoppingCart.vue/ShoppingCart.vue";
+import ShoppingCart from "../components/shoppingCart/ShoppingCart.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
